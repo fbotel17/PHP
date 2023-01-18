d4ccefe105ccd9433f082d9bc32b3a2056977dd4
fbotel17 <faustbtl17@gmail.com>
2023-01-18 14:24:53 +0100

    test

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <a href="exo1.php">exo1</a>
+    <a href="exo2-1.php">exo2.1</a>
+    <a href="exo2-2.php">exo2.2</a>
+    <a href="exo3.php">exo3</a>
+    <a href="exo4.php">exo4</a>
+    <a href="exo5.php">exo5</a>
+    <a href="exo6.php">exo6</a>
+    <a href="exo7.php">exo7</a>
+    <a href="exo8.php">exo8</a>
+    <a href="final.php">exoFinal</a>
+    <a href="testBDD.php">testBDD</a>
+</body>
+</html>
