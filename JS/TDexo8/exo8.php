cc6efab8e54b2907c1b20642bcbca6da679334e6
fbotel17 <faustbtl17@gmail.com>
2023-03-11 09:44:53 +0100

    exo8

---
+++ JS/TDexo8/exo8.php
@@ -0,0 +1,16 @@
+<!-- exo8 -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Exo8</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <div onmouseout="this.style.color='black'" onmouseover="this.style.color='red'" onclick="this.style.color='lime'" ondblclick="this.style.color='navy'">Cliquez sur le boutton</div>
+</body>
+<script type="text/javascript" src="main.js"></script>
+
+</html>
